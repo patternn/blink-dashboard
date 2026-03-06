befb8eae51f61a61ba4f7c340796d0456cd3abb2
Pattern <146864611+patternn@users.noreply.github.com>
2026-03-06 12:02:29 -0600

    Delete frontend-addition directory

--- frontend-addition/frontend/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { AnimatedNumber } from "./AnimatedNumber";
-export { Sparkline } from "./Sparkline";
-export { StatCard } from "./StatCard";
-export { LivePulse } from "./LivePulse";
-export { GlobeIndicator } from "./GlobeIndicator";
-export { SkeletonCard } from "./SkeletonCard";
